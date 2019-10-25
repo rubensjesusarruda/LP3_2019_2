93f07eee6c34b66cfdf57108f8540393593746ad
Rubens Jesus <rubens.arruda@hotmail.com>
2019-10-24 20:55:04 -0400

    Iniciando o projeto

---
+++ tv-search-pwa/src/Router.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import{
+    BrowserRouter,
+    Switch,
+    Route
+}from 'react-router-dom';
+
+import Pesquisar from './pages/Pesquisar';
+import VisualizarSerie from './pages/VisualizarSerie'
+
+const Router = () =>(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Pesquisar}/>
+            <Route exact path="/serie" component={VisualizarSerie}/>
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Router;
